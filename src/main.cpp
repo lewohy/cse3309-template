c5d2e201743c27da841537eca4894abebea39df6
lewohy <lwh8762@gmail.com>
2025-09-04 13:24:04 +0000

    .

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int a, b;
+
+    std::cin >> a >> b;
+    std::cout << a + b << std::endl;
 
     return 0;
 }
