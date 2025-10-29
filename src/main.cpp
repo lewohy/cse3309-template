73004c0bc2e1c40378c3098961b094daa4869afb
cse3309 <c@s.e>
2025-10-29 04:53:56 +0000

    .

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,9 @@ int main() {
     int a, b;
 
     std::cin >> a >> b;
-    std::cout << a + b << std::endl;
+    std::cout << a + b << "\n";
+
+    std::cerr << a << " + " << b << "\n";
 
     return 0;
 }
